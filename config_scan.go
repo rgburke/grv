d6f8fe4ef65a076c6ce37ff3ea8a28f0cc6da0a5
Richard Burke <rich.g.burke@gmail.com>
2017-03-08 21:25:05 +0000

    Added config scanner

---
+++ config_scan.go
@@ -0,0 +1,339 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"unicode"
+)
+
+type TokenType int
+
+const (
+	TK_INVALID TokenType = iota
+	TK_WORD
+	TK_OPTION
+	TK_WHITE_SPACE
+	TK_TERMINATOR
+	TK_EOF
+)
+
+type ScannerPos struct {
+	line uint
+	col  uint
+}
+
+type Token struct {
+	tokenType TokenType
+	value     string
+	startPos  ScannerPos
+	endPos    ScannerPos
+	err       error
+}
+
+type Scanner struct {
+	reader          *bufio.Reader
+	pos             ScannerPos
+	lastCharLineEnd bool
+	lastLineEndCol  uint
+}
+
+func (token *Token) Equal(other *Token) bool {
+	if other == nil {
+		return false
+	}
+
+	return token.tokenType == other.tokenType &&
+		token.value == other.value &&
+		token.startPos == other.startPos &&
+		token.endPos == other.endPos &&
+		((token.err == nil && other.err == nil) ||
+			(token.err != nil && other.err != nil &&
+				token.err.Error() == other.err.Error()))
+}
+
+func NewScanner(reader io.Reader) *Scanner {
+	return &Scanner{
+		reader: bufio.NewReader(reader),
+		pos: ScannerPos{
+			line: 1,
+			col:  0,
+		},
+	}
+}
+
+func (scanner *Scanner) read() (char rune, eof bool, err error) {
+	char, _, err = scanner.reader.ReadRune()
+
+	if err == io.EOF {
+		eof = true
+		err = nil
+
+		if scanner.pos.col == 0 {
+			scanner.pos.col = 1
+		}
+	} else if err == nil {
+		if scanner.lastCharLineEnd {
+			scanner.lastLineEndCol = scanner.pos.col
+			scanner.pos.line++
+			scanner.pos.col = 1
+		} else {
+			scanner.pos.col++
+		}
+
+		scanner.lastCharLineEnd = (char == '\n')
+	}
+
+	return
+}
+
+func (scanner *Scanner) unread() (err error) {
+	if err = scanner.reader.UnreadRune(); err != nil {
+		return
+	}
+
+	if scanner.pos.line > 1 && scanner.pos.col == 1 {
+		scanner.pos.line--
+		scanner.pos.col = scanner.lastLineEndCol
+		scanner.lastCharLineEnd = true
+	} else {
+		scanner.pos.col--
+		scanner.lastCharLineEnd = false
+	}
+
+	return
+}
+
+func (scanner *Scanner) Scan() (token *Token, err error) {
+	char, eof, err := scanner.read()
+	startPos := scanner.pos
+
+	switch {
+	case err != nil:
+	case eof:
+		token = &Token{
+			tokenType: TK_EOF,
+			endPos:    scanner.pos,
+		}
+	case char == '\n':
+		token = &Token{
+			tokenType: TK_TERMINATOR,
+			value:     string(char),
+			endPos:    scanner.pos,
+		}
+	case unicode.IsSpace(char):
+		if err = scanner.unread(); err != nil {
+			break
+		}
+
+		token, err = scanner.scanWhiteSpace()
+	case char == '-':
+		var nextBytes []byte
+		nextBytes, err = scanner.reader.Peek(1)
+
+		if err != nil {
+			break
+		} else if len(nextBytes) == 1 && nextBytes[0] == '-' {
+			token, err = scanner.scanWord()
+
+			if token != nil && token.tokenType != TK_INVALID {
+				token.tokenType = TK_OPTION
+				token.value = "-" + token.value
+			}
+
+			break
+		}
+
+		if err = scanner.unread(); err != nil {
+			break
+		}
+
+		token, err = scanner.scanWord()
+	case char == '"':
+		if err = scanner.unread(); err != nil {
+			break
+		}
+
+		token, err = scanner.scanStringWord()
+	default:
+		if err = scanner.unread(); err != nil {
+			break
+		}
+
+		token, err = scanner.scanWord()
+	}
+
+	if token != nil {
+		token.startPos = startPos
+	}
+
+	return
+}
+
+func (scanner *Scanner) scanWhiteSpace() (token *Token, err error) {
+	var buffer bytes.Buffer
+	var char rune
+	var eof bool
+
+	escape := false
+
+OuterLoop:
+	for {
+		char, eof, err = scanner.read()
+
+		switch {
+		case err != nil:
+			return
+		case eof:
+			break OuterLoop
+		case char == '\\':
+			var nextBytes []byte
+			nextBytes, err = scanner.reader.Peek(1)
+
+			if err != nil {
+				return
+			} else if len(nextBytes) == 1 && nextBytes[0] == '\n' {
+				escape = true
+				continue
+			}
+
+			if err = scanner.unread(); err != nil {
+				return
+			}
+
+			break OuterLoop
+		case char == '\n':
+			if !escape {
+				if err = scanner.unread(); err != nil {
+					return
+				}
+
+				break OuterLoop
+			}
+		case !unicode.IsSpace(char):
+			if err = scanner.unread(); err != nil {
+				return
+			}
+
+			break OuterLoop
+		default:
+			if _, err = buffer.WriteRune(char); err != nil {
+				return
+			}
+		}
+
+		escape = false
+	}
+
+	token = &Token{
+		tokenType: TK_WHITE_SPACE,
+		value:     buffer.String(),
+		endPos:    scanner.pos,
+	}
+
+	return
+}
+
+func (scanner *Scanner) scanWord() (token *Token, err error) {
+	var buffer bytes.Buffer
+	var char rune
+	var eof bool
+
+OuterLoop:
+	for {
+		char, eof, err = scanner.read()
+
+		switch {
+		case err != nil:
+			return
+		case eof:
+			break OuterLoop
+		case unicode.IsSpace(char):
+			if err = scanner.unread(); err != nil {
+				return
+			}
+
+			break OuterLoop
+		default:
+			if _, err = buffer.WriteRune(char); err != nil {
+				return
+			}
+		}
+	}
+
+	token = &Token{
+		tokenType: TK_WORD,
+		value:     buffer.String(),
+		endPos:    scanner.pos,
+	}
+
+	return
+}
+
+func (scanner *Scanner) scanStringWord() (token *Token, err error) {
+	var buffer bytes.Buffer
+	var char rune
+	var eof bool
+
+	char, eof, err = scanner.read()
+	if _, err = buffer.WriteRune(char); err != nil {
+		return
+	}
+
+	closingQuoteFound := false
+	escape := false
+
+OuterLoop:
+	for {
+		char, eof, err = scanner.read()
+
+		switch {
+		case err != nil:
+			return
+		case eof:
+			break OuterLoop
+		case char == '\\':
+			if _, err = buffer.WriteRune(char); err != nil {
+				return
+			}
+
+			if !escape {
+				escape = true
+				continue
+			}
+		case char == '"':
+			if _, err = buffer.WriteRune(char); err != nil {
+				return
+			}
+
+			if !escape {
+				closingQuoteFound = true
+				break OuterLoop
+			}
+		default:
+			if _, err = buffer.WriteRune(char); err != nil {
+				return
+			}
+		}
+
+		escape = false
+	}
+
+	if closingQuoteFound {
+		token = &Token{
+			tokenType: TK_WORD,
+			value:     buffer.String(),
+			endPos:    scanner.pos,
+		}
+	} else {
+		token = &Token{
+			tokenType: TK_INVALID,
+			value:     buffer.String(),
+			endPos:    scanner.pos,
+			err:       errors.New("Unterminated string"),
+		}
+	}
+
+	return
+}
---
+++ config_scan_test.go
@@ -0,0 +1,566 @@
+package main
+
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestScanSingleTokens(t *testing.T) {
+	var singleTokenTests = []struct {
+		input         string
+		expectedToken Token
+	}{
+		{
+			input: "-!\"word1世界",
+			expectedToken: Token{
+				tokenType: TK_WORD,
+				value:     "-!\"word1世界",
+				startPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+				endPos: ScannerPos{
+					line: 1,
+					col:  10,
+				},
+			},
+		},
+		{
+			input: "\"word \\\"with\\\" spaces\"",
+			expectedToken: Token{
+				tokenType: TK_WORD,
+				value:     "\"word \\\"with\\\" spaces\"",
+				startPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+				endPos: ScannerPos{
+					line: 1,
+					col:  22,
+				},
+			},
+		},
+		{
+			input: " \t\r\v\f",
+			expectedToken: Token{
+				tokenType: TK_WHITE_SPACE,
+				value:     " \t\r\v\f",
+				startPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+				endPos: ScannerPos{
+					line: 1,
+					col:  5,
+				},
+			},
+		},
+		{
+			input: "--option",
+			expectedToken: Token{
+				tokenType: TK_OPTION,
+				value:     "--option",
+				startPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+				endPos: ScannerPos{
+					line: 1,
+					col:  8,
+				},
+			},
+		},
+		{
+			input: "\n",
+			expectedToken: Token{
+				tokenType: TK_TERMINATOR,
+				value:     "\n",
+				startPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+				endPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+			},
+		},
+		{
+			input: "",
+			expectedToken: Token{
+				tokenType: TK_EOF,
+				startPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+				endPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+			},
+		},
+		{
+			input: "\"Unterminated string",
+			expectedToken: Token{
+				tokenType: TK_INVALID,
+				value:     "\"Unterminated string",
+				startPos: ScannerPos{
+					line: 1,
+					col:  1,
+				},
+				endPos: ScannerPos{
+					line: 1,
+					col:  20,
+				},
+				err: errors.New("Unterminated string"),
+			},
+		},
+	}
+
+	for _, singleTokenTest := range singleTokenTests {
+		scanner := NewScanner(strings.NewReader(singleTokenTest.input))
+		token, err := scanner.Scan()
+
+		if err != nil {
+			t.Errorf("Scan failed with error %v", err)
+		} else if !token.Equal(&singleTokenTest.expectedToken) {
+			t.Errorf("Token does not match expected value. Expected %v, Actual %v", singleTokenTest.expectedToken, *token)
+		}
+	}
+}
+
+func TestScanMultipleTokens(t *testing.T) {
+	var multiTokenTests = []struct {
+		input          string
+		expectedTokens []Token
+	}{
+		{
+			input: "theme --create \"my theme\"\n",
+			expectedTokens: []Token{
+				Token{
+					tokenType: TK_WORD,
+					value:     "theme",
+					startPos: ScannerPos{
+						line: 1,
+						col:  1,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  5,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 1,
+						col:  6,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  6,
+					},
+				},
+				Token{
+					tokenType: TK_OPTION,
+					value:     "--create",
+					startPos: ScannerPos{
+						line: 1,
+						col:  7,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  14,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 1,
+						col:  15,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  15,
+					},
+				},
+				Token{
+					tokenType: TK_WORD,
+					value:     "\"my theme\"",
+					startPos: ScannerPos{
+						line: 1,
+						col:  16,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  25,
+					},
+				},
+				Token{
+					tokenType: TK_TERMINATOR,
+					value:     "\n",
+					startPos: ScannerPos{
+						line: 1,
+						col:  26,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  26,
+					},
+				},
+				Token{
+					tokenType: TK_EOF,
+					startPos: ScannerPos{
+						line: 1,
+						col:  26,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  26,
+					},
+				},
+			},
+		},
+		{
+			input: "set theme mytheme\nset\tCommitView.dateformat \"%yyyy-mm-dd HH:MM\"\n",
+			expectedTokens: []Token{
+				Token{
+					tokenType: TK_WORD,
+					value:     "set",
+					startPos: ScannerPos{
+						line: 1,
+						col:  1,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  3,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 1,
+						col:  4,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  4,
+					},
+				},
+				Token{
+					tokenType: TK_WORD,
+					value:     "theme",
+					startPos: ScannerPos{
+						line: 1,
+						col:  5,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  9,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 1,
+						col:  10,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  10,
+					},
+				},
+				Token{
+					tokenType: TK_WORD,
+					value:     "mytheme",
+					startPos: ScannerPos{
+						line: 1,
+						col:  11,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  17,
+					},
+				},
+				Token{
+					tokenType: TK_TERMINATOR,
+					value:     "\n",
+					startPos: ScannerPos{
+						line: 1,
+						col:  18,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  18,
+					},
+				},
+				Token{
+					tokenType: TK_WORD,
+					value:     "set",
+					startPos: ScannerPos{
+						line: 2,
+						col:  1,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  3,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     "\t",
+					startPos: ScannerPos{
+						line: 2,
+						col:  4,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  4,
+					},
+				},
+				Token{
+					tokenType: TK_WORD,
+					value:     "CommitView.dateformat",
+					startPos: ScannerPos{
+						line: 2,
+						col:  5,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  25,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 2,
+						col:  26,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  26,
+					},
+				},
+				Token{
+					tokenType: TK_WORD,
+					value:     "\"%yyyy-mm-dd HH:MM\"",
+					startPos: ScannerPos{
+						line: 2,
+						col:  27,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  45,
+					},
+				},
+				Token{
+					tokenType: TK_TERMINATOR,
+					value:     "\n",
+					startPos: ScannerPos{
+						line: 2,
+						col:  46,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  46,
+					},
+				},
+				Token{
+					tokenType: TK_EOF,
+					startPos: ScannerPos{
+						line: 2,
+						col:  46,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  46,
+					},
+				},
+			},
+		},
+		{
+			input: "theme --create \\\n\tmytheme\n",
+			expectedTokens: []Token{
+				Token{
+					tokenType: TK_WORD,
+					value:     "theme",
+					startPos: ScannerPos{
+						line: 1,
+						col:  1,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  5,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 1,
+						col:  6,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  6,
+					},
+				},
+				Token{
+					tokenType: TK_OPTION,
+					value:     "--create",
+					startPos: ScannerPos{
+						line: 1,
+						col:  7,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  14,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " \t",
+					startPos: ScannerPos{
+						line: 1,
+						col:  15,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  1,
+					},
+				},
+				Token{
+					tokenType: TK_WORD,
+					value:     "mytheme",
+					startPos: ScannerPos{
+						line: 2,
+						col:  2,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  8,
+					},
+				},
+				Token{
+					tokenType: TK_TERMINATOR,
+					value:     "\n",
+					startPos: ScannerPos{
+						line: 2,
+						col:  9,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  9,
+					},
+				},
+				Token{
+					tokenType: TK_EOF,
+					startPos: ScannerPos{
+						line: 2,
+						col:  9,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  9,
+					},
+				},
+			},
+		},
+		{
+			input: "theme --create \"my theme\nset theme mytheme\n",
+			expectedTokens: []Token{
+				Token{
+					tokenType: TK_WORD,
+					value:     "theme",
+					startPos: ScannerPos{
+						line: 1,
+						col:  1,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  5,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 1,
+						col:  6,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  6,
+					},
+				},
+				Token{
+					tokenType: TK_OPTION,
+					value:     "--create",
+					startPos: ScannerPos{
+						line: 1,
+						col:  7,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  14,
+					},
+				},
+				Token{
+					tokenType: TK_WHITE_SPACE,
+					value:     " ",
+					startPos: ScannerPos{
+						line: 1,
+						col:  15,
+					},
+					endPos: ScannerPos{
+						line: 1,
+						col:  15,
+					},
+				},
+				Token{
+					tokenType: TK_INVALID,
+					value:     "\"my theme\nset theme mytheme\n",
+					startPos: ScannerPos{
+						line: 1,
+						col:  16,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  18,
+					},
+					err: errors.New("Unterminated string"),
+				},
+				Token{
+					tokenType: TK_EOF,
+					startPos: ScannerPos{
+						line: 2,
+						col:  18,
+					},
+					endPos: ScannerPos{
+						line: 2,
+						col:  18,
+					},
+				},
+			},
+		},
+	}
+
+	for _, multiTokenTest := range multiTokenTests {
+		scanner := NewScanner(strings.NewReader(multiTokenTest.input))
+
+		for _, expectedToken := range multiTokenTest.expectedTokens {
+			token, err := scanner.Scan()
+
+			if err != nil {
+				t.Errorf("Scan failed with error %v", err)
+			} else if !token.Equal(&expectedToken) {
+				t.Errorf("Token does not match expected value. Expected %v, Actual %v", expectedToken, *token)
+			}
+		}
+	}
+}
