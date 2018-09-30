18646aec7b86ebb4dda77a3f7c0b67c235bfa741
Richard Burke <rich.g.burke@gmail.com>
2018-09-30 15:25:50 +0100

    Use custom vertical split ratio in summary view

--- cmd/grv/summary_view.go
+++ cmd/grv/summary_view.go
@@ -2,7 +2,8 @@ package main
 
 const (
 	// SummaryViewTitle is the title of the Summary View
-	SummaryViewTitle = "Summary View"
+	SummaryViewTitle            = "Summary View"
+	svVerticalSplitOffsetFactor = 0.15
 )
 
 // NewSummaryView creates a new instance
@@ -12,10 +13,31 @@ func NewSummaryView(repoData RepoData, repoController RepoController, channels C
 
 	summaryView := NewContainerView(channels, config)
 	summaryView.SetWindowStyleConfig(NewWindowStyleConfig(false, SrsUnderline))
+	summaryView.SetChildViewPositionCalculator(&summaryViewPositionCalculator{summaryView})
 	summaryView.SetTitle(SummaryViewTitle)
 	summaryView.SetOrientation(CoDynamic)
 	summaryView.SetViewID(ViewSummary)
 	summaryView.AddChildViews(gitSummaryView, childViewContainer)
 
 	return summaryView
+}
+
+type summaryViewPositionCalculator struct {
+	summaryView *ContainerView
+}
+
+// CalculateChildViewPositions calculates the child layout data for the summary view
+func (calculator *summaryViewPositionCalculator) CalculateChildViewPositions(viewLayoutData *ViewLayoutData) (childPositions []*ChildViewPosition) {
+	childPositions = calculator.summaryView.CalculateChildViewPositions(viewLayoutData)
+	childPositionNum := uint(len(childPositions))
+
+	if !viewLayoutData.fullScreen && viewLayoutData.orientation == CoVertical && childPositionNum == 2 {
+		offset := uint(float64(childPositions[0].viewDimension.cols) * svVerticalSplitOffsetFactor)
+
+		childPositions[0].viewDimension.cols -= offset
+		childPositions[1].viewDimension.cols += offset
+		childPositions[1].startCol -= offset
+	}
+
+	return
 }
